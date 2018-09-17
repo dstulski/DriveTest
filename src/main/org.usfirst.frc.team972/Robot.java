50de0962dab593dcbd7d77b2ced0ff040f9730ae
unknown <ethana2442@gmail.com>
2018-09-17 15:48:04 -0700

    first commit

---
+++ src/main/org.usfirst.frc.team972/Robot.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team972;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.*;
+
+public class Robot extends IterativeRobot {
+	WPI_TalonSRX r1 = new WPI_TalonSRX(1);
+	WPI_TalonSRX r2 = new WPI_TalonSRX(2);
+	WPI_TalonSRX r3 = new WPI_TalonSRX(3);
+	WPI_TalonSRX l1 = new WPI_TalonSRX(4);
+	WPI_TalonSRX l2 = new WPI_TalonSRX(5);
+	WPI_TalonSRX l3 = new WPI_TalonSRX(6);
+	
+	double rightSpeed;
+	double leftSpeed;
+	
+	Joystick joy = new Joystick(0);
+
+	@Override
+	public void robotInit() {
+		
+	}
+	@Override
+	public void teleopInit() {
+
+	}
+
+	@Override
+	public void teleopPeriodic() {
+		rightSpeed.set(joy.getRawAxis(5));
+		leftSpeed.set(joy.getRawAxis(1));
+		r1.set(ControlMode.PercentError, rightSpeed);
+		r2.set(ControlMode.PercentError, rightSpeed);
+		r3.set(ControlMode.PercentError, rightSpeed);
+		l1.set(ControlMode.PercentError, -leftSpeed);
+		l2.set(ControlMode.PercentError, -leftSpeed);
+		l3.set(ControlMode.PercentError, -leftSpeed);
+	}
+
+}
